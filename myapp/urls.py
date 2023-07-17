1ac4041e5ffe5ea071ebc2c6ba96cea1cc228b2d
Inwerejosic <inwerejosic@protonmail.com>
2023-07-17 10:13:10 +0100

    Now worked further on the views

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.home,),
+    path('about', views.about,),
+    path('menu', views.menu,),
+    path('book', views.book, name='book'),
+    path('modify_str', views.modify_str, name='modify_str'),
     path('drinks/<str:drink_name>', views.drinks),
     path('drinks/', views.drinks),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -28,3 +28,18 @@ def drinks(request,): #drink_name):
     # return HttpResponse(f"<h2>{drink_name}</h2> " + choice_of_drink)
     return HttpResponse(f"<h1>{zx_type}</h1>")
 
+def about(request):
+    return HttpResponse('<h1>About us</h1>')  
+
+
+def menu(request):
+    return HttpResponse('<h1>Menu</h1>')  
+
+
+def book(request):
+    return HttpResponse('<h1>Make a booking</h1>')    
+
+def modify_str(request):
+    str1 = 'I love Python'
+    str2 = str1.replace("love", "enjoy").split()
+    return HttpResponse(f"<center><h2>{str2}</h2></center>")
